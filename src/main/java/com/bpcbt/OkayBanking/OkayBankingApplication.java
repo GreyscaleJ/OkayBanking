2b115999499491949c1f59ac22cea1b1318a4d5a
GreyscaleJ <vsevolod.illichov.v@gmail.com>
2018-11-21 01:25:11 +0200

    initial commit

---
+++ src/main/java/com/bpcbt/OkayBanking/OkayBankingApplication.java
@@ -0,0 +1,12 @@
+package com.bpcbt.OkayBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkayBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkayBankingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bpcbt/OkayBanking/OkayBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bpcbt.OkayBanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OkayBankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
